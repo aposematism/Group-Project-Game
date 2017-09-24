f15af1146ca1af3fe57613f4609542f37af90c39
Aposematism <Aposematism@Aposematism-PC>
2017-09-24 19:38:58 +1300

    test

--- src/parser/TerrianParser.java
+++ src/parser/TerrianParser.java
@@ -1,4 +1,17 @@
+package parser;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class TerrianParser {
 
+	/** 
+	 * This class is the terrian parser, for parsing any objects which do not interact with the player, NPCs or enemies.
+	 * I.E. Walls, floors, Trees, Cliffs etc.
+	 * @author Jordan
+	 * */
+	
+	public void init_scanner(){
+		BufferedReader buffRoad = new BufferedReader(new FileReader(region));
+	}
 }
