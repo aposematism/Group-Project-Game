4983d16a8e9bd515b3bd1f2e4652b9374d3e761c
Dylan McKay <me@dylanmckay.io>
2017-09-27 00:58:06 +1300

    Remove now-redundant methods from the Graph class
    These were in the original design, but do not make much sense anymore.

--- src/pathfinding/Graph.java
+++ src/pathfinding/Graph.java
@@ -4,8 +4,7 @@ import java.util.Collection;
 import java.util.Optional;
 
 import model.Region;
-import util.Direction;
-import util.GridLocation;
+import model.Tile;
 
 /**
  * A grid-based graph that can be used to find shortest paths.
@@ -21,62 +20,6 @@ public class Graph {
 		this.region = region;
 	}
 
-	/**
-	 * Places a node at a location.
-	 */
-    void set(GridLocation location, Node node) {
-    	// FIXME: implement
-    }
-
-    /**
-     * Gets the node at a given location.
-     */
-    Optional<Node> get(GridLocation location) {
-    	// FIXME: implement
-    	return null;
-    }
-
-    /**
-     * Clears the node at a location.
-     */
-    // FIXME: we may not need this if we are given the grid in the constructor
-    Optional<Node> unset(GridLocation l) {
-    	// FIXME: implement
-    	return null;
-    }
-
-    /**
-     * Checks if two nodes are adjacent
-     */
-    boolean isAdjacent(Node a, Node b) {
-    	// FIXME: implement
-    	return false;
-    }
-
-    /**
-     * Moves a node to another location
-     */
-    // FIXME: we may not need this if we are given the grid in the constructor
-    void move(Node node, GridLocation location) {
-    	// FIXME: implement
-    }
-
-    /**
-     * Moves a node in a direction
-     */
-    // FIXME: we may not need this if we are given the grid in the constructor
-    void move(Node node, Direction direction) {
-    	// FIXME: implement
-    }
-
-    /**
-     * Gets all neighbors adjacent to a node.
-     */
-    Collection<Node> getNeighbours(Node node) {
-    	// FIXME: implement
-    	return null;
-    }
-
     /**
      * Finds the most optimal path between two nodes
      * 
@@ -87,21 +30,5 @@ public class Graph {
     Optional<Path> findPath(Node a, Node b) {
     	// FIXME: implement
     	return null;
-    }
-    
-    public Region getRegion() { return this.region; }
-
-    /**
-     * Gets the width of the graph grid.
-     */
-    int getWidth() {
-    	return -1; // FIXME: implement
-    }
-
-    /**
-     * Gets the height of the graph grid.
-     */
-    int getHeight() {
-    	return -1; // FIXME: implement
     }
 }
