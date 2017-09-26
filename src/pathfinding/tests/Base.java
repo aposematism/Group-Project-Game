b458e9f60562b81273237805ad52d0be18ee3d5e
Dylan McKay <me@dylanmckay.io>
2017-09-27 04:22:21 +1300

    Extend the Graph class so that it handles unreachable paths

--- src/pathfinding/tests/Base.java
+++ src/pathfinding/tests/Base.java
@@ -17,14 +17,25 @@ public class Base {
      * Verifies that the pathfinder returns a path between two tiles.
      */
     protected void verifyPath(PathfindingGrid grid, Tile source, Tile dest, GridLocation ...expectedPathParts) {
-        Graph graph = new Graph(grid, source, dest);
-
         Path expectedPath = new Path(Arrays.asList(expectedPathParts));
-        Optional<Path> actualPath = graph.findPath();
+        Optional<Path> actualPath = getPath(grid, source, dest);
         
         if (!actualPath.isPresent())
             fail(String.format("expected '%s' but no path was possible", expectedPath));
         
         assertEquals(expectedPath, actualPath.get());
+    }
+    
+    protected void verifyUnreachable(PathfindingGrid grid, Tile source, Tile dest) {
+        Optional<Path> actualPath = getPath(grid, source, dest);
+        if (actualPath.isPresent())
+            fail("there should be no path between the source and destination");
+    }
+    
+    private Optional<Path> getPath(PathfindingGrid grid, Tile source, Tile dest) {
+        Graph graph = new Graph(grid, source, dest);
+
+        Optional<Path> actualPath = graph.findPath();
+        return actualPath;
     }
 }
--- src/pathfinding/tests/BasicTests.java
+++ src/pathfinding/tests/BasicTests.java
@@ -1,5 +1,7 @@
 package pathfinding.tests;
 
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
@@ -31,6 +33,39 @@ public class BasicTests extends Base {
         verifyPath(grid, source, dest,
                 new GridLocation(0, 0), new GridLocation(1, 1), new GridLocation(2, 1),
                 new GridLocation(3, 2), new GridLocation(4, 3), new GridLocation(4, 4));
+    }
+
+    /**
+     * Looks like
+     * 
+     * |-----------|
+     * | A   w     |
+     * |     w     |
+     * | w w w w w |
+     * |     w   B |
+     * |-----------|
+     * 
+     * No path is possible.
+     */
+    @Test
+    public void returnsNoPathWhenUnreachable() {
+        TestGrid grid = new TestGrid(5, 5);
+        Tile source = grid.get(0, 0);
+        Tile dest = grid.get(4, 4);
+
+        // Draw two lines right through the map to cut the dest off.
+        {
+            for (int y=0; y<grid.height; y++) {
+                Tile tile = grid.get(2, y);
+                tile.setMapEntity(new Obstacle(tile.getLocation(), null));
+            }
+            for (int x=0; x<grid.width; x++) {
+                Tile tile = grid.get(x, 2);
+                tile.setMapEntity(new Obstacle(tile.getLocation(), null));
+            }
+        }
+        
+        verifyUnreachable(grid, source, dest);
     }
     
     // There was a point where routing was indeterministic because hashCode was not implemented
