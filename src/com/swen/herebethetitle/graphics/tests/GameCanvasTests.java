5a4546c4f31df304270ebf8f1c80431e3f614683
Mark Metcalfe <mark.ls.metcalfe@gmail.com>
2017-10-01 15:39:45 +1300

    Revert accidental rollback

--- src/com/swen/herebethetitle/graphics/tests/GameCanvasTests.java
+++ src/com/swen/herebethetitle/graphics/tests/GameCanvasTests.java
@@ -1,11 +1,12 @@
 package com.swen.herebethetitle.graphics.tests;
 
 import com.swen.herebethetitle.entity.items.Item;
+import com.swen.herebethetitle.entity.items.Key;
 import com.swen.herebethetitle.graphics.GridManager;
+import com.swen.herebethetitle.model.GameContext;
 import com.swen.herebethetitle.util.Direction;
 import com.swen.herebethetitle.entity.Entity;
 import com.swen.herebethetitle.entity.Player;
-import com.swen.herebethetitle.entity.Terrain;
 import com.swen.herebethetitle.graphics.GameCanvas;
 import com.swen.herebethetitle.model.Region;
 import com.swen.herebethetitle.model.Tile;
@@ -25,114 +26,121 @@ import org.junit.Assert.*;
 import static org.junit.Assert.fail;
 
 public class GameCanvasTests{
-    static Operation testMethod;
-    static TestStage ts;
+	static Operation testMethod;
+	static TestStage ts;
 
-    static boolean failTestMethod = false;
+	static boolean failTestMethod = false;
 
-    @BeforeClass
-    public static void runBeforeTest(){
-        ts = new TestStage();
-    }
+	@BeforeClass
+	public static void runBeforeTest(){
+		ts = new TestStage();
+	}
 
-    @AfterClass
-    public static void runAfterTest() {
-        try {
-            ts.startWindow();
-        }catch(RuntimeException E){
-            if(!failTestMethod) {
-                failTestMethod = true;
-                fail(E.getMessage());
-            }
-        }
-    }
+	@AfterClass
+	public static void runAfterTest() {
+		try {
+			ts.startWindow();
+		}catch(RuntimeException E){
+			if(!failTestMethod) {
+				failTestMethod = true;
+				fail(E.getMessage());
+			}
+		}
+	}
 
-    @Test
-    public void test01_initialize(){
-        testMethod = new Operation(){public void run(GameCanvas c){
-           c.drawAll();
-        }};
-        update(ts);
-    }
+	@Test
+	public void test01_initialize(){
+		testMethod = new Operation(){public void run(GameCanvas c){
+			c.drawAll();
+		}};
+		update(ts);
+	}
 
-    @Test
-    public void test02_switchCanvas(){
-        testMethod = new Operation(){public void run(GameCanvas c){
-            c.drawAll();
-            c.switchRegions(TestStage.emptyGrassField(5,1, 0, 0));
-            c.drawAll();
-        }};
-        update(ts);
-    }
+	@Test
+	public void test02_switchCanvas(){
+		testMethod = new Operation(){public void run(GameCanvas c){
+			c.drawAll();
+			c.switchRegions(TestStage.emptyGrassField(5,1, 0, 0));
+			c.drawAll();
+		}};
+		update(ts);
+	}
 
-    @Test
-    public void test03_CanvasWithoutPlayer(){
-        failTestMethod = true;
-        try {
-            testMethod = new Operation() {
-                public void run(GameCanvas c) {
-                    c.drawAll();
-                    c.switchRegions(TestStage.playerless(5, 1));
-                    c.drawAll();
-                }
-            };
-            update(ts);
-            //fail("shouldn't be able to switch to region without player");
-        } catch(RuntimeException e){}
-    }
+	@Test
+	public void test03_CanvasWithoutPlayer(){
+		failTestMethod = true;
+		try {
+			testMethod = new Operation() {
+				public void run(GameCanvas c) {
+					c.drawAll();
+					c.switchRegions(TestStage.playerless(5, 1));
+					c.drawAll();
+				}
+			};
+			update(ts);
+			//fail("shouldn't be able to switch to region without player");
+		} catch(RuntimeException e){}
+	}
 
-    @Test
-    public void test04_translated(){
-        testMethod = new Operation(){public void run(GameCanvas c){
-            c.drawAll();
-            c.switchRegions(TestStage.emptyGrassField(5,5, 4, 3));
-            c.drawAll();
-        }};
-        update(ts);
-    }
+	@Test
+	public void test04_translated(){
+		testMethod = new Operation(){public void run(GameCanvas c){
+			c.drawAll();
+			c.switchRegions(TestStage.emptyGrassField(5,5, 4, 3));
+			c.drawAll();
+		}};
+		update(ts);
+	}
 
-    @Test
-    public void test05_changeGridManager(){
-        testMethod = new Operation(){public void run(GameCanvas c){
-            c.drawAll();
-            c.setGrid(new GridManager(40, 2,2));
-            c.drawAll();
-        }};
-        update(ts);
-    }
+	@Test
+	public void test05_changeGridManager(){
+		testMethod = new Operation(){public void run(GameCanvas c){
+			c.drawAll();
+			c.setGrid(new GridManager(40, 2,2));
+			c.drawAll();
+		}};
+		update(ts);
+	}
 
-    @Test
-    public void test06_ModifyGridManager(){
-        testMethod = new Operation(){public void run(GameCanvas c){
-            c.drawAll();
-            c.getGrid().setCellSize(10);
-            c.getGrid().sethGap(5);
-            c.drawAll();
-        }};
-        update(ts);
-    }
+	@Test
+	public void test06_ModifyGridManager(){
+		testMethod = new Operation(){public void run(GameCanvas c){
+			c.drawAll();
+			c.getGrid().setCellSize(10);
+			c.getGrid().sethGap(5);
+			c.drawAll();
+		}};
+		update(ts);
+	}
 
-    @Test
-    public void test07_ModifyRegion(){
-        testMethod = new Operation(){public void run(GameCanvas c){
-            Tile tile = c.getRegion().get(0,1);
-            Entity extra = new Item(new Image("file:res/cobble snow.png"));
-            tile.add(extra);
-            c.drawAll();
-        }};
-        update(ts);
-    }
+	@Test
+	public void test07_ModifyRegion(){
+		testMethod = new Operation(){public void run(GameCanvas c){
+			Tile tile = c.getRegion().get(0,1);
+			Entity extra = new DummyEntity(new Image("file:res/cobble snow.png"));
+			tile.add(extra);
+			c.drawAll();
+		}};
+		update(ts);
+	}
 
-    @Test
-    public void test08_StackedEntities(){
-        testMethod = new Operation(){public void run(GameCanvas c){
-            c.switchRegions(TestStage.grasslandWithExtras(2,2,0,0));
-            c.drawAll();
-        }};
-        update(ts);
-    }
+	@Test
+	public void test08_StackedEntities(){
+		testMethod = new Operation(){public void run(GameCanvas c){
+			c.switchRegions(TestStage.grasslandWithExtras(2,2,0,0));
+			c.drawAll();
+		}};
+		update(ts);
+	}
 
-    private void update(TestStage ts){
-        ts.setTestMethod(testMethod);
-    }
+	private void update(TestStage ts){
+		ts.setTestMethod(testMethod);
+	}
+
+	public static class DummyEntity extends Entity{
+		public boolean isPenetrable(){ return false; }
+		public void interact(GameContext context){}
+		public String toString(){return null;}
+		public DummyEntity(Image image){super(image);}
+	}
 }
--- src/com/swen/herebethetitle/graphics/tests/TestStage.java
+++ src/com/swen/herebethetitle/graphics/tests/TestStage.java
@@ -1,10 +1,10 @@
 package com.swen.herebethetitle.graphics.tests;
 
 import com.swen.herebethetitle.entity.Entity;
+import com.swen.herebethetitle.entity.Floor;
 import com.swen.herebethetitle.entity.items.Item;
 import com.swen.herebethetitle.util.Direction;
 import com.swen.herebethetitle.entity.Player;
-import com.swen.herebethetitle.entity.Terrain;
 import com.swen.herebethetitle.graphics.GameCanvas;
 import com.swen.herebethetitle.model.Region;
 import com.swen.herebethetitle.model.Tile;
@@ -16,79 +16,79 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class TestStage extends Application {
-    private static Image grass;
-    private static Image player;
-    private static Image tudorwall;
+	private static Image grass;
+	private static Image player;
+	private static Image tudorwall;
 
-    private GameCanvas currentCanvas;
-    private static Operation testMethod;
+	private GameCanvas currentCanvas;
+	private static Operation testMethod;
 
-    public TestStage(){ }
+	public TestStage(){ }
 
-    public void setTestMethod(Operation o){
-        testMethod = o;
-    }
+	public void setTestMethod(Operation o){
+		testMethod = o;
+	}
 
-    public void startWindow(){
-        launch();
-    }
+	public void startWindow(){
+		launch();
+	}
 
-    public void runTestMethod(){
-        testMethod.run(currentCanvas);
-    }
-
-
-    public void start(Stage stage){
-        grass = new Image("file:res/grass.png");
-        player = new Image("file:res/wizard.png");
-        tudorwall = new Image("file:res/tudorwall.png");
-
-        currentCanvas = new GameCanvas(emptyGrassField(7,7, 0, 0), 350, 350);
-
-        stage.setTitle("Test");
-        StackPane root = new StackPane();
-        root.getChildren().add(currentCanvas);
-        stage.setScene(new Scene(root));
-        stage.show();
-
-        testMethod.run(currentCanvas);
-    }
+	public void runTestMethod(){
+		testMethod.run(currentCanvas);
+	}
 
 
+	public void start(Stage stage){
+		grass = new Image("file:res/grass.png");
+		player = new Image("file:res/wizard.png");
+		tudorwall = new Image("file:res/tudorwall.png");
 
-    public static Region emptyGrassField(int cols, int rows, int playerCol, int playerRow){
-        Region r = new Region(cols, rows);
+		currentCanvas = new GameCanvas(emptyGrassField(7,7, 0, 0), 350, 350);
 
-        for(int row=0;row<rows;row++) {
-            for (int col = 0; col < cols; col++) {
-                Tile t = new Tile(col, row, "");
-                t.setMapTerrain(new Terrain(grass));
-                r.set(new GridLocation(col,row), t);
-            }
-        }
-        r.get(playerCol,playerRow).add(new Player(player, Direction.Right));
-        return r;
-    }
+		stage.setTitle("Test");
+		StackPane root = new StackPane();
+		root.getChildren().add(currentCanvas);
+		stage.setScene(new Scene(root));
+		stage.show();
 
-    public static Region grasslandWithExtras(int cols, int rows, int playerCol, int playerRow){
-        Region r = emptyGrassField(cols, rows, 0,0);
+		testMethod.run(currentCanvas);
+	}
 
-        r.get(0,1).add(new Item(tudorwall));
-        r.get(0, 1).add(new Item(player));
 
-        return r;
-    }
 
-    public static Region playerless(int cols, int rows){
-        Region r = emptyGrassField(cols, rows, 0, 0);
-        Player p = null;
+	public static Region emptyGrassField(int cols, int rows, int playerCol, int playerRow){
+		Region r = new Region(cols, rows);
 
-        for(Entity e: r.getPlayerTile().getInteractives()){
-            if(e instanceof Player) p = (Player)e;
-        }
+		for(int row=0;row<rows;row++) {
+			for (int col = 0; col < cols; col++) {
+				Tile t = new Tile(col, row, "");
+				t.setMapFloor(new Floor(grass));
+				r.set(new GridLocation(col,row), t);
+			}
+		}
+		r.get(playerCol,playerRow).add(new Player(player, Direction.Right));
+		return r;
+	}
 
-        r.remove(p);
-        return r;
-    }
+	public static Region grasslandWithExtras(int cols, int rows, int playerCol, int playerRow){
+		Region r = emptyGrassField(cols, rows, 0,0);
+
+		r.get(0,1).add(new GameCanvasTests.DummyEntity(tudorwall));
+		r.get(0, 1).add(new GameCanvasTests.DummyEntity(player));
+
+		return r;
+	}
+
+	public static Region playerless(int cols, int rows){
+		Region r = emptyGrassField(cols, rows, 0, 0);
+		Player p = null;
+
+		for(Entity e: r.getPlayerTile().getInteractives()){
+			if(e instanceof Player) p = (Player)e;
+		}
+
+		r.remove(p);
+		return r;
+	}
 
 }
