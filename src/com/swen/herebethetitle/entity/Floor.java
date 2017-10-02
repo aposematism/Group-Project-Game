a677bf76dacd0dd89003e3c60a803e8f0a2e8e77
Dylan McKay <me@dylanmckay.io>
2017-10-02 18:51:37 +1300

    Revert "Make floors impenetrable"
    This reverts commit e83c85d990d7042d60fb7fd55ed2e68b21711c65.

--- src/com/swen/herebethetitle/entity/Floor.java
+++ src/com/swen/herebethetitle/entity/Floor.java
@@ -25,7 +25,7 @@ public class Floor extends Entity {
 	 * need to be able to sit on top of it
 	 */
 	@Override
-    public boolean isPenetrable() { return false; }
+    public boolean isPenetrable() { return true; }
 
     @Override
     public String toString() { return super.toString(); }
