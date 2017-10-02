8163b31f6f26ecfb6dc0cc978bf90e4c4a90be27
Mark Metcalfe <mark.ls.metcalfe@gmail.com>
2017-10-02 19:27:01 +1300

    Fixed Parser to work with updated Entity library and toString() representations

--- src/com/swen/herebethetitle/parser/parsertests/InteractiveParserTests.java
+++ src/com/swen/herebethetitle/parser/parsertests/InteractiveParserTests.java
@@ -2,7 +2,12 @@ package com.swen.herebethetitle.parser.parsertests;
 
 import static org.junit.Assert.fail;
 
+import com.swen.herebethetitle.entity.Player;
+import com.swen.herebethetitle.parser.InteractiveParser;
+import com.swen.herebethetitle.util.Direction;
 import org.junit.Test;
+
+import java.util.Scanner;
 
 public class InteractiveParserTests {
 	public class TerrainParserTests {
@@ -14,4 +19,12 @@ public class InteractiveParserTests {
 		}
 	}
 
+//	@Test
+//	public void test_Player_input(){
+//		Player p = new Player(null, Direction.Down);
+//		System.out.println(p.toString());
+//		InteractiveParser i = new InteractiveParser();
+//		Scanner s = new Scanner(p.toString());
+//		Player p2 = i.parsePlayer(s);
+//	}
 }
