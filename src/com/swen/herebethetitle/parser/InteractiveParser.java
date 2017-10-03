a91c123e97ea3fb86258c95bb832787208847041
Aposematism <Aposematism@Aposematism-PC>
2017-10-04 02:14:24 +1300

    Interactive Parser reworking in progress to enable hardcoding for item traits.

--- src/com/swen/herebethetitle/parser/InteractiveParser.java
+++ src/com/swen/herebethetitle/parser/InteractiveParser.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -32,6 +33,23 @@ public class InteractiveParser {
 	final static Pattern ITEM = Pattern.compile("(Weapon|Armour|Potion|Key)");
 
 	ArrayList<String> stringArray;
+	HashMap<String, String> spriteMap;
+	
+	/** 
+	 * So this hardcodes the string for each item. It ensures the level designer only needs to give
+	 * an item name and co-ordinates to generate an item or a level.
+	 * */
+	public InteractiveParser() {
+		spriteMap = new HashMap<String, String>();
+		spriteMap.put("weapon", "path/to/sprite.png true 5");
+		spriteMap.put("helmet", "path/to/sprite.png HELMET 5");
+		spriteMap.put("boots", "path/to/sprite.png BOOTS 5");
+		spriteMap.put("legs", "path/to/sprite.png LEGS 5");
+		spriteMap.put("torso", "path/to/sprite.png TORSO 5");
+		spriteMap.put("key", "path/to/sprite.png true 5");
+		spriteMap.put("potion", "path/to/sprite.png true 5");
+		spriteMap.put("static", "path/to/sprite.png true 5");
+	}
 
 	/**
 	 * This method is for the initialization of the region file.
@@ -59,21 +77,32 @@ public class InteractiveParser {
 
 	/**
 	 * This method does actually process the line in the interactive file for each item it intends to load.
-	 * @author - Jordan
+	 * Mostly unused after hardcoded map update.
+	 * @author - Jordan Milburn
 	 * */
 	public void parseStringArray(Region reg){
 		for(String s : stringArray){
 			Scanner scanner = new Scanner(s);
-
 			int x = scanner.nextInt();
 			int y = scanner.nextInt();
 			Tile t = reg.get(x, y);
-
 			String className = scanner.next();
 			Entity item = parseInteractive(scanner, className);
-
 			t.add(item);
 		}
+	}
+	/** 
+	 * Same as above, but enables independent testing without a region.
+	 * @Author - Jordan Milburn
+	 * */
+	public Entity parseStringArray(String s) {
+		Scanner scanner = new Scanner(s);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		//Tile t = reg.get(x, y);
+		String className = scanner.next();
+		Entity item = parseInteractive(scanner, className);
+		return item;
 	}
 
 	/**
@@ -83,93 +112,150 @@ public class InteractiveParser {
 	 * */
 	public Entity parseInteractive(Scanner scanner, String className){
 		switch(className){
-			case "Static": return parseStatic(scanner);
+			case "door":	   return parseStatic(scanner, className);
+			case "static":	   return parseStatic(scanner, className);
 			case "Player":     return parsePlayer(scanner);
 			case "NPC":        return parseNPC(scanner);
 			default:           return parseItem(scanner, className);
 		}
 	}
 
+	/** 
+	 * This handles strings and creates the appropriate string for the objects at hand.
+	 * @author - Mark Metcalfe
+	 * */
 	public String parseString(Scanner s){
 		String string = "";
 		String next = s.next(".");
-		while(!next.equals("\"")){
+		while(!next.equals(".")){
 			string = string.concat(next);
 			next = s.next(".");
 		}
 		return string;
 	}
-
+	
+	/** 
+	 * Checking against spriteMap hashmap for the image. So it checks if that item is there and returns a matching path string.
+	 * */
+	public String parseString(String item_name) {
+		if(spriteMap.containsKey(item_name)) {
+			return spriteMap.get(item_name);
+		}
+		return null;
+	}
+	
+	/** 
+	 * This method handles the items.
+	 * @author - Mark Metcalfe
+	 * */
 	public Item parseItem(Scanner s){
 		String className = s.next();
 		return parseItem(s, className);
 	}
-
+	/** 
+	 * This method parses onto the appropriate category.
+	 * @author - Mark Metcalfe
+	 * */
 	public Item parseItem(Scanner s, String className){
 		switch (className){
-			case "Weapon": return parseWeapon(s);
-			case "Armour": return parseArmour(s);
-			case "Key":    return parseKey(s);
-			case "Potion": return parsePotion(s);
+			case "weapon": return parseWeapon(s, className);
+			case "helmet": return parseArmour(s, className);
+			case "torso": return parseArmour(s, className);
+			case "boots": return parseArmour(s, className);
+			case "legs": return parseArmour(s, className);
+			case "key":    return parseKey(s, className);
+			case "potion": return parsePotion(s, className);
 			default:       throw new InputMismatchException("Couldn't Interpret Entity");
 		}
 	}
-
-	public Weapon parseWeapon(Scanner s){
-		String name = parseString(s);
-		String sprite = parseString(s);
-		boolean isMelee = Boolean.parseBoolean(s.next());
-		double strength = Double.parseDouble(s.next());
-
+	
+	/** 
+	 * This method parses weapons. Takes from the hardcoded spritemap which contains relevant info on the item.
+	 * @author - Mark Metcalfe and Jordan Milburn
+	 * */
+	public Weapon parseWeapon(Scanner s, String className){
+		String name = className;
+		String returned = parseString(name);
+		String[] split = returned.split(" ");
+		String sprite = split[0];
+		boolean isMelee = Boolean.parseBoolean(split[1]);
+		double strength = Double.parseDouble(split[2]);
 		return new Weapon(name, sprite, isMelee, strength);
 	}
-
-	public Armour parseArmour(Scanner s){
-		String name = parseString(s);
-		String sprite = parseString(s);
-		Armour.TYPE type = Armour.TYPE.valueOf(s.next());
-		double strength = Double.parseDouble(s.next());
-
+	
+	/** 
+	 * This method parses armour.
+	 * @author - Mark Metcalfe and Jordan Milburn
+	 * */
+	public Armour parseArmour(Scanner s, String className){
+		String name = className;
+		String returned = parseString(name);
+		String[] split = returned.split(" ");
+		String sprite = split[0];
+		Armour.TYPE type = Armour.TYPE.valueOf(split[1]);
+		double strength = Double.parseDouble(split[0]);
 		return new Armour(name, sprite, type, strength);
 	}
-
-	public Key parseKey(Scanner s){
-		String name = parseString(s);
-		String sprite = parseString(s);
-		int key = Integer.parseInt(s.next());
-
+	
+	/** 
+	 * This method parses keys.
+	 * @author - Mark Metcalfe and Jordan Milburn
+	 * */
+	public Key parseKey(Scanner s, String className){
+		String name = className;
+		String returned = parseString(name);
+		String[] split = returned.split(" ");
+		String sprite = split[0];
+		int key = Integer.parseInt(split[1]);
 		return new Key(name, sprite, key);
 	}
-
-	public Potion parsePotion(Scanner s){
-		String name = parseString(s);
-		String sprite = parseString(s);
-		int value = Integer.parseInt(s.next());
-
+	
+	/** 
+	 * This method parses potions.
+	 * @author - Mark Metcalfe and Jordan Milburn
+	 * */ 
+	public Potion parsePotion(Scanner s, String className){
+		String name = className;
+		String returned = parseString(name);
+		String[] split = returned.split(" ");
+		String sprite = split[0];
+		int value = Integer.parseInt(split[1]);
 		return new Potion(name, sprite, value);
 	}
-
-	public Static parseStatic(Scanner s){
-		String name = parseString(s);
-		String sprite = parseString(s);
-
+	
+	/** 
+	 * This method parses statics like Doors.
+	 * @author - Mark Metcalfe and Jordan Milburn
+	 * */
+	public Static parseStatic(Scanner s, String className){
+		String name = className;
+		String returned = parseString(name);
+		String[] split = returned.split(" ");
+		String sprite = split[0];
 		Static aStatic = new Static(name, sprite);
-
-		if(s.hasNext("Door")){
+		if(split[1].equals("Door")){
 			s.next(); //Consume "Door" token
-			Door door = parseDoor(s);
+			Door door = parseDoor(split);
 			aStatic.setBehavior(door);
 		}
 
 		return aStatic;
 	}
-
-	public Door parseDoor(Scanner s){
-		int key = Integer.parseInt(s.next());
-		Door.STATE state = Door.STATE.valueOf(s.next());
+	
+	/** 
+	 * This method parses doors explicitly for generating a key.
+	 * @author - Mark Metcalfe and Jordan Milburn
+	 * */
+	public Door parseDoor(String[] s){
+		int key = Integer.parseInt(s[2]);
+		Door.STATE state = Door.STATE.valueOf(s[3]);
 		return new Door(key, state);
 	}
-
+	
+	/** 
+	 * This method parses NPCs.
+	 * @author - Mark Metcalfe and Jordan Milburn
+	 * */
 	public NPC parseNPC(Scanner s){
 		String name = parseString(s);
 		String sprite = parseString(s);
@@ -186,13 +272,21 @@ public class InteractiveParser {
 
 		return npc;
 	}
-
+	
+	/** 
+	 * This method parses monsters.
+	 * @author - Mark Metcalfe
+	 * */
 	public Monster parseMonster(Scanner s){
 		double strength = Double.parseDouble(s.next());
 
 		return new Monster(strength);
 	}
-
+	
+	/** 
+	 * This method parses players.
+	 * @author - Mark Metcalfe
+	 * */
 	public Player parsePlayer(Scanner s){
 		String sprite = parseString(s);
 		double health = Double.parseDouble(s.next());
--- src/com/swen/herebethetitle/parser/parsertests/InteractiveParserTests.java
+++ src/com/swen/herebethetitle/parser/parsertests/InteractiveParserTests.java
@@ -1,24 +1,23 @@
 package com.swen.herebethetitle.parser.parsertests;
 
-import static org.junit.Assert.fail;
-
+import com.swen.herebethetitle.entity.Entity;
 import com.swen.herebethetitle.entity.Player;
+import com.swen.herebethetitle.entity.items.Weapon;
 import com.swen.herebethetitle.parser.InteractiveParser;
 import com.swen.herebethetitle.util.Direction;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 import java.util.Scanner;
 
 public class InteractiveParserTests {
-	public class TerrainParserTests {
-		public class PlayerTests {
 			@Test
-			public void test_1(){
-				fail("Not Implemented");
+			public void test_generic_item_creation(){
+				InteractiveParser ip = new InteractiveParser();
+				Entity e = ip.parseStringArray("0 0 weapon true 5");
+				assertTrue(e instanceof Weapon);
 			}
-		}
-	}
-
 //	@Test
 //	public void test_Player_input(){
 //		Player p = new Player(null, Direction.Down);
