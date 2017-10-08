8a6bcf99d3c80dfc8aa3976848d7ff01c89a754f
Dylan McKay <me@dylanmckay.io>
2017-10-08 19:13:10 +1300

    Clean up the PlayerMove interaction
    The file mixed spaces and tabs, which makes it very hard to read on many
    editors.
    
    I've also made the fields private and changed the file to use a single
    comment style.

--- src/com/swen/herebethetitle/logic/ai/PlayerMove.java
+++ src/com/swen/herebethetitle/logic/ai/PlayerMove.java
@@ -9,8 +9,6 @@ import com.swen.herebethetitle.model.Tile;
 import com.swen.herebethetitle.pathfinding.Graph;
 import com.swen.herebethetitle.pathfinding.Path;
 
-import javafx.scene.input.MouseEvent;
-
 
 /**
  * Interaction to move the player.
@@ -18,38 +16,36 @@ import javafx.scene.input.MouseEvent;
  *
  */
 public class PlayerMove implements Interaction {
-	
-	public Player player;
-	public Tile dest;
+    private Player player;
+    private Tile dest;
 
-	@Override
-	public void tick(Region region, Notifier notifier) throws InteractionOver {
-		/*find optimal path*/
+    @Override
+    public void tick(Region region, Notifier notifier) throws InteractionOver {
+        /* find optimal path */
         Graph graph = new Graph(region, region.getPlayerTile(), dest);
         Optional<Path> optimalPath = graph.findPath();
-        
-        if(!optimalPath.isPresent()) {
-        	//no optimal path exists, do nothing
-        	throw new InteractionOver();
-        }
-        
-        /*Move the player*/
-        if(!(region.getPlayerTile()==dest)) {	//don't move if we're already there
-        	region.move(player, optimalPath.get().next());
-        }else {
-        	throw new InteractionOver();
-        }
-	}
 
-	@Override
-	public boolean isSameAs(Interaction interaction) {
+        if(!optimalPath.isPresent()) {
+            // no optimal path exists, do nothing
+            throw new InteractionOver();
+        }
+
+        // Move the player
+        if(!(region.getPlayerTile()==dest)) { //don't move if we're already there
+            region.move(player, optimalPath.get().next());
+        }else {
+            throw new InteractionOver();
+        }
+    }
+
+    @Override
+    public boolean isSameAs(Interaction interaction) {
         if(!(interaction instanceof PlayerMove)) {
-        	return false;
+            return false;
         }
         if(((PlayerMove)interaction).dest==this.dest) {
-        	return true;
+            return true;
         }
         return false;
-	}
-
+    }
 }
