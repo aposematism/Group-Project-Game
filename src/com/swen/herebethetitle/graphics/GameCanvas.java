2391dd279386f196008cfe96aabecb14006fefe7
Josh <weirjosh>
2017-10-15 14:54:49 +1300

    Fixes rows and columns being mixed up in Map parser

--- src/com/swen/herebethetitle/graphics/GameCanvas.java
+++ src/com/swen/herebethetitle/graphics/GameCanvas.java
@@ -219,8 +219,7 @@ public class GameCanvas extends Canvas implements GameListener {
 
 	@Override
 	public void onPlayerAttacked(Player player, NPC attacker) {
-		
-	}
+    }
 
 	@Override
 	public void onPlayerKilled(Player player, Optional<NPC> aggressor) {
