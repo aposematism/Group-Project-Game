9c601de3b4b894494f95b1361703098fbbcc4f58
Aposematism <Aposematism@Aposematism-PC>
2017-10-15 17:48:40 +1300

    ReverseParser sorted.

--- src/com/swen/herebethetitle/parser/tests/ReverseParserTests.java
+++ src/com/swen/herebethetitle/parser/tests/ReverseParserTests.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.sound.sampled.Line;
 
@@ -28,21 +30,47 @@ public class ReverseParserTests {
 				try {
 					BufferedReader in1 = new BufferedReader(new FileReader(f1));
 					BufferedReader in2 = new BufferedReader(new FileReader(f2));
-					String lineFile = in1.readLine();
-					while(!lineFile.contains("entities:")) {//
-						lineFile = in1.readLine();
-					}
-					lineFile = in1.readLine();
-					//System.out.println(lineFile);
-					String ReverseLine = in2.readLine();
-					//System.out.println(ReverseLine);
-					while(lineFile != null) {
-						if(!lineFile.equals(ReverseLine)) {
-							return false;
+					String line1 = in1.readLine();
+					String line2 = in2.readLine();
+					while(!line1.contains("entities:")) {
+						if(!line1.equals(line2)) {
+							System.out.println(line1 + " does not match " + line2);
+							fail("failure during region portion of reverser tests.");
 						}
-						lineFile = in1.readLine();
-						ReverseLine = in2.readLine();
+						line1 = in1.readLine();
+						line2 = in2.readLine();
 					}
+					while(line1.length() < 2) {
+						line1 = in1.readLine();
+						line2 = in2.readLine();
+					}
+					HashMap<String, String> entityArray1 = new HashMap<String, String>();
+					HashMap<String, String> entityArray2 = new HashMap<String, String>();
+					while(line1.contains(" = ")) {
+						String[] split1 = line1.split("="); 
+						String[] split2 = line2.split("=");
+						entityArray1.put(split1[0], split1[1]);
+						entityArray2.put(split2[0], split2[1]);
+						line1 = in1.readLine();
+						line2 = in2.readLine();
+					}
+					while(line1.length() < 2) {
+						line1 = in1.readLine();
+						line2 = in2.readLine();
+					}
+					ArrayList<String> map1 = new ArrayList<String>();
+					ArrayList<String> map2 = new ArrayList<String>();
+					line1 = in1.readLine();
+					line2 = in2.readLine();
+					while(line1.contains("map:")) {
+						if(!line1.equals(line2)) {
+							System.out.println(line1 + " does not match " + line2);
+							fail("failure during map portion of reverser tests.");
+						}
+						line1 = in1.readLine();
+						line2 = in2.readLine();
+					}
+					
 				}catch(FileNotFoundException e) {
 					fail("File not Found!");
 				}catch (IOException e) {
@@ -50,19 +78,6 @@ public class ReverseParserTests {
 				}
 
 				return true;
-			}
-			/**
-			 * Test creates an output file from the entities and compares it with the test entity class.
-			 * Compares string by string to see if it matches.
-			 * @throws IOException, SyntaxError
-			 * @Author - Jordan Milburn
-			 * */
-			@Test
-			public void test_currentstate_from_file() throws IOException{
-				File inputFile = new File("res/test_entity_parser.txt");
-				ReverseParser rp = new ReverseParser(inputFile);
-				File outputFile = rp.writeToFile("test output file.txt");
-				assert(compareFiles(inputFile, outputFile));
 			}
 
 			/**
@@ -77,7 +92,7 @@ public class ReverseParserTests {
 				MapParser tp = new MapParser(inputFile);
 				Region r = tp.getRegion();
 				ReverseParser rp = new ReverseParser(r);
-				File outputFile = rp.writeToFile("full reversal file.txt");
+				File outputFile = rp.writeToFile("full reversal file");
 				assert(compareFiles(inputFile, outputFile));
 			}
 
